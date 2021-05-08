45e2fd93432b5fc9e862a5a9ef0ef7b017e51f3d
Gaurav-Thuwal <66351039+Gaurav-Thuwal@users.noreply.github.com>
2021-05-08 18:37:15 +0530

    $ git commit -m

---
+++ config.php
@@ -0,0 +1,12 @@
+<!-- Created by : Gaurav Thuwal -->
+
+<?php
+
+	//$conn = mysqli_connect("localhost","root","","sparks-bank(9828)");
+	$conn = mysqli_connect("localhost","id16777015_gaurav","C{=9SXJ9ekYH|!tr","id16777015_sparks_bank");
+
+	if(!$conn){
+		die("Could not connect to the database due to the following error --> ".mysqli_connect_error());
+	}
+
+?>
